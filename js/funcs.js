b4bca3d62d740e4f299c15d1c82d17f2eccf9f00
rildexter <rildexter@hotmail.com>
2021-11-11 10:22:56 -0300

    atualizado com partes feitas por alunos

---
+++ js/funcs.js
@@ -0,0 +1,45 @@
+    function clicando1(){
+        var a = parseFloat(document.getElementById("a").value)
+        document.getElementById("receber1").innerHTML = (a**2/Math.sqrt(3)).toFixed(2)
+    }
+	    function clicando2(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        document.getElementById("receber2").innerHTML = (a**2/Math.sqrt(3)*b).toFixed(2)
+    }
+	    function clicando3(){
+        var a = parseFloat(document.getElementById("a").value)
+        document.getElementById("receber3").innerHTML = (3*(2*a)/Math.sqrt(3)).toFixed(2)
+    }
+	    function clicando4(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        document.getElementById("receber4").innerHTML = (a*b/2).toFixed(2)
+    }
+	    function clicando5(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        var c = parseFloat(document.getElementById("c").value)
+        document.getElementById("receber5").innerHTML = (a*b/2*c).toFixed(2)
+    }
+	    function clicando6(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        document.getElementById("receber6").innerHTML = (2*Math.sqrt((a**2)+(b/2)**+a+b)).toFixed(2)
+    }
+	    function clicando7(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        document.getElementById("receber7").innerHTML = (a*b/2).toFixed(2)
+    }
+	    function clicando8(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        var c = parseFloat(document.getElementById("c").value)
+        document.getElementById("receber8").innerHTML = ((a*b/2)*c).toFixed(2)
+    }
+	    function clicando9(){
+        var a = parseFloat(document.getElementById("a").value)
+        var b = parseFloat(document.getElementById("b").value)
+        document.getElementById("receber9").innerHTML = (2*Math.sqrt((a**2)+(b**2)+a+b)).toFixed(2)
+    }
